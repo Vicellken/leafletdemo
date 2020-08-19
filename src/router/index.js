65838e7386320a5c0165a611d6380163979ed0bd
Yifei <gulivecassie@gmail.com>
2020-08-19 17:16:12 +0800

    vue leaflet demo

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,20 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
+    path: '/map',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import(/* webpackChunkName: "about" */ '../views/leafletDemo.vue')
     }
   }
 ]
